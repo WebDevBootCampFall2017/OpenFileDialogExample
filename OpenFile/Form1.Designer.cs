8e840c4dc4d4a1a45656b670445aec5805e69e73
LabUser <LabUser@waketech.edu>
2017-08-12 10:34:29 -0400

    first commit

---
+++ OpenFile/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace OpenFile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.tbFileOutput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(302, 148);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(55, 13);
+            this.linkLabel1.TabIndex = 0;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "linkLabel1";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // tbFileOutput
+            // 
+            this.tbFileOutput.Location = new System.Drawing.Point(12, 12);
+            this.tbFileOutput.Multiline = true;
+            this.tbFileOutput.Name = "tbFileOutput";
+            this.tbFileOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbFileOutput.Size = new System.Drawing.Size(632, 115);
+            this.tbFileOutput.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(656, 170);
+            this.Controls.Add(this.tbFileOutput);
+            this.Controls.Add(this.linkLabel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox tbFileOutput;
+    }
+}
+
---
+++ OpenFile/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace OpenFile
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Event handler for a click on our link
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            //Step 1:  Find File
+            openFileDialog1.ShowDialog();
+        }
+
+        //Event Handler for a successful file selection
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            //Get the filename from the open file dialog
+            string s = openFileDialog1.FileName;
+
+            //Read the file data
+            string file_data = File.ReadAllText(s);
+
+            //Display out file data
+            tbFileOutput.Text = file_data;
+        }
+    }
+}
